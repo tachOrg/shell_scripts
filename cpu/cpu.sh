c353bb418509c7484fb411839f2d94d561d7e2df
Luis Fernando Campos Mendoza <lucampos2305@gmail.com>
2023-04-19 21:57:48 -0600

    CPU stats commands added

--- cpu/cpu.sh
+++ cpu/cpu.sh
@@ -9,6 +9,12 @@ date_time=$(date +"%Y-%m-%d %H:%M:%S.$miliseconds")
 echo "Current date & time: $date_time"
 
 # Get CPU usage statistics
-cpu=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+cpu1=$(top -bn1 | grep load)
+cpu=$(top -bn1 | grep load | awk '{printf "%.2f%%\n", $(NF-2)}')
+cpus=$(top -bn1 | grep 'Cpu(s)')
+sum=$(top -bn1 | grep "Cpu(s)" | awk -v n=$(nproc) '{sum=0; for(i=2;i<=n+1;i++){sum+=$i}; printf "%.2f%%\n", sum}')
 
+echo "CPU: $cpu1"
 echo "CPU Usage: $cpu"
+echo "CPUs: $cpus"
+echo "Sum: $sum"
