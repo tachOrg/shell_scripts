47e137f6825a91b009dea20fd71ead66d3429ecf
Pablo Anastacio Mora Chatski <panastacio@dotdashmdp.com>
2023-04-19 20:46:04 -0600

    output iftop to output file

--- network_bandwidth/iftop.sh
+++ network_bandwidth/iftop.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo iftop -i wlp0s20f3 -P -o iftop_o
+
+# visualize iftop dynamically
+# sudo iftop -i wlp0s20f3 -P -o iftop_o
+
+# iftop to output file
+sudo iftop -i wlp0s20f3 -t -L 10 -B > output
+
